75948a5cc020c7e058d8bd8c626bfc589abeeba3
Caio Lopes <caiolopesfv@gmail.com>
2024-08-26 20:36:33 -0300

    Add exceptions

---
+++ src/exceptions/not-found-exception.ts
@@ -0,0 +1,8 @@
+import { AppException } from "./app-exception";
+
+export class NotFoundException extends AppException {
+    constructor(entity: string) {
+        super(`${entity} not found`)
+    }
+}
+
---
+++ src/exceptions/unauthorization-exception.ts
@@ -0,0 +1,8 @@
+
+import { AppException } from "./app-exception";
+
+export class Unauthorization extends AppException {
+    constructor() {
+        super(`User without permission.`)
+    }
+}
