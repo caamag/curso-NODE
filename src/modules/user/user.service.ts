1faf0ebb89a7e12fe011d8cecd0863f66089eb4e
Caio Lopes <caiolopesfv@gmail.com>
2024-08-26 21:21:40 -0300

    Submiting new content

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { UserModel } from "./user.model";
 import { UserInsertDTO } from "./dtos/user-insert.dto";
+import { NotFoundException } from "@exceptions/not-found-exception";
+
 const prisma = new PrismaClient();
 
 export const getUsers = async (): Promise<UserModel[]> => {
+    const users = await prisma.user.findMany()
+
+    if (users?.length === 0) {
+        throw new NotFoundException('User')
+    }
+
     return prisma.user.findMany();
 }
 
